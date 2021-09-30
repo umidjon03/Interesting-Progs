93e9db377022d9f070c7cf23a0532fe2701ccd27
Umidjon <shuxratovumidjon03@gmail.com>
2021-09-30 17:07:21 +0500

    Create virus.py

---
+++ Covid 19/virus.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Sep 30 16:29:45 2021
+
+@author: asus
+"""
+
+from turtle import *
+speed = 100
+color('cyan')
+bgcolor('black')
+b = 200
+while b>0:
+    left(b)
+    forward(b * 3)
+    b = b - 1
